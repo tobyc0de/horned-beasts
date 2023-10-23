946072f4c59b0bdfe404c8643ccb5f1ab4a473ab
Tobias Foit <tobias.foit@gmail.com>
2023-10-23 14:07:12 +0100

    asd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Grid from "./components/Grid";
+import Row from "./components/Row";
+import Square from "./components/Square";
+
+function App() {
+  return (
+    <div>
+      <h1>Bla a grid!</h1>
+      <Grid />
+    </div>
+  );
+}
+
+export default App;
